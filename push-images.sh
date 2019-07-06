690bc8c7a533800750c2c7ce64cb60e955c1128f
Chen Weiguang <chen.weiguang@gmail.com>
2019-07-06 23:58:17 +0800

    Remove latest push and fix shellcheck lints on script

--- push-images.sh
+++ push-images.sh
@@ -3,24 +3,22 @@ set -euo pipefail
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
-export RUST_DATE=$(docker run -it $IMAGE_NAME rustc --version |
+RUST_DATE=$(docker run -it "$IMAGE_NAME" rustc --version |
     grep -oE "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}")
 
-export RUST_VER=$(docker run -it $IMAGE_NAME rustc --version |
+RUST_VER=$(docker run -it "$IMAGE_NAME" rustc --version |
     grep -oE "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]")
 
-docker push $IMAGE_NAME
-
 TAG1="$RUST_VER-$RUST_CHANNEL"
-docker tag $IMAGE_NAME $IMAGE_NAME:$TAG1
-docker push $IMAGE_NAME:$TAG1
+docker tag "$IMAGE_NAME" "$IMAGE_NAME:$TAG1"
+docker push "$IMAGE_NAME:$TAG1"
 
 TAG2="$RUST_CHANNEL"
-docker tag $IMAGE_NAME $IMAGE_NAME:$TAG2
-docker push $IMAGE_NAME:$TAG2
+docker tag "$IMAGE_NAME" "$IMAGE_NAME:$TAG2"
+docker push "$IMAGE_NAME:$TAG2"
 
 if [ "$RUST_CHANNEL" = "nightly" ]; then
     TAG3="$RUST_CHANNEL-$RUST_DATE"
-    docker tag $IMAGE_NAME $IMAGE_NAME:$TAG3
-    docker push $IMAGE_NAME:$TAG3
+    docker tag "$IMAGE_NAME" "$IMAGE_NAME:$TAG3"
+    docker push "$IMAGE_NAME:$TAG3"
 fi 
